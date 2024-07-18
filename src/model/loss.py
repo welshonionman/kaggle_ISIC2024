ac7834affd4701e84e0ae20e06e534287e4f8027
welshonionman <welshonionman@gmail.com>
2024-07-18 16:29:53 +0000

    weighted lossfn

--- src/model/loss.py
+++ src/model/loss.py
@@ -1,8 +1,10 @@
+import torch
 import torch.nn as nn
 
 
 def get_lossfn(cfg):
+    weights = torch.tensor(cfg.loss_weight).cuda()
     if cfg.lossfn == "BCELoss":
-        return nn.BCELoss()
+        return nn.BCELoss(weight=weights)
     else:
         raise ValueError(f"Invalid Loss Function: {cfg.lossfn}")
