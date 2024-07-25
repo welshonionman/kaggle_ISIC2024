5f2950ab261b897ed7e2aab2c2f969ef1e1d9706
welshonionman <welshonionman@gmail.com>
2024-07-25 12:42:58 +0000

    aux pipeline

---
+++ src/pipeline/aux.py
@@ -0,0 +1,179 @@
+from pathlib import Path
+
+import numpy as np
+import torch
+import torch.nn as nn
+import wandb
+from tqdm import tqdm
+
+from src.constants import COMP_NAME, DEVICE
+from src.dataset import get_test_dataloader, get_train_dataloader
+from src.model import get_lossfn, get_model, get_optimizer, get_scheduler
+from src.preprocess import get_infer_preprocess, get_train_preprocess
+
+from .metrics import calc_score
+from .utils import set_seed
+
+
+def pbar_train_desc(pbar_train, scheduler, epoch, total_epoch, average_loss):
+    learning_rate = f"LR : {scheduler.get_last_lr()[0]:.2E}"
+    gpu_memory = f"Mem : {torch.cuda.memory_reserved() / 1E9:.3g}GB"
+    epoch_info = f"Epoch {epoch}/{total_epoch}"
+    loss = f"Loss: {average_loss:.4f}"
+
+    description = f"{epoch_info:12} {gpu_memory:15} {learning_rate:15} {loss:15}"
+    pbar_train.set_description(description)
+
+
+def pbar_valid_desc(pbar_val, average_loss):
+    average_val_loss = f"Val Loss: {average_loss:.4f}"
+
+    description = f"{average_val_loss:18}"
+    pbar_val.set_description(description)
+
+
+def train_1epoch(model, train_loader, optimizer, scheduler, epoch, cfg):
+    train_loss = 0
+
+    model.train()
+
+    pbar_train = enumerate(train_loader)
+    pbar_train = tqdm(
+        pbar_train,
+        total=len(train_loader),
+        bar_format="{l_bar}{bar:10}{r_bar}{bar:-0b}",
+    )
+
+    for batch_idx, batch in pbar_train:
+        inputs = batch["image"].to(DEVICE, dtype=torch.float)
+
+        labels = batch["target"].to(DEVICE, dtype=torch.float)[:, [0]]
+        aux_labels = batch["target"].to(DEVICE, dtype=torch.float)[:, 1:]
+        optimizer.zero_grad()
+
+        outputs = model(inputs)["malignant"]
+
+        # criterion = get_lossfn(cfg, labels)
+        criterion = nn.BCEWithLogitsLoss()
+        aux_criterion_1 = nn.BCEWithLogitsLoss()
+
+        loss = criterion(outputs[:, [0]], labels)
+        loss_1 = aux_criterion_1(outputs[:, [1]], aux_labels)
+        loss_ = loss + loss_1
+        loss_.backward()
+
+        optimizer.step()
+
+        train_loss += loss_.item()
+        average_loss = train_loss / (batch_idx + 1)
+
+        pbar_train_desc(pbar_train, scheduler, epoch, cfg.epochs, average_loss)
+    scheduler.step()
+    return average_loss
+
+
+def valid_1epoch(model, valid_loader, epoch, cfg):
+    valid_loss = 0
+    y_true = torch.tensor([], device=DEVICE)
+    all_outputs = torch.tensor([], device=DEVICE)
+    model.eval()
+
+    pbar_val = enumerate(valid_loader)
+    pbar_val = tqdm(
+        pbar_val,
+        total=len(valid_loader),
+        bar_format="{l_bar}{bar:10}{r_bar}{bar:-10b}",
+    )
+
+    with torch.inference_mode():
+        for batch_idx, batch in pbar_val:
+            inputs = batch["image"].to(DEVICE, dtype=torch.float)
+            labels = batch["target"].to(DEVICE, dtype=torch.float)[:, [0]]
+            aux_labels = batch["target"].to(DEVICE, dtype=torch.float)[:, 1:]
+
+            outputs = model(inputs)["malignant"].squeeze()
+
+            # criterion = get_lossfn(cfg, labels)
+            criterion = nn.BCEWithLogitsLoss()
+            aux_criterion_1 = nn.BCEWithLogitsLoss()
+
+            loss = criterion(outputs[:, [0]], labels).item()
+            loss_1 = aux_criterion_1(outputs[:, [1]], aux_labels).item()
+            loss_ = loss + loss_1
+            valid_loss += loss_
+            y_true = torch.cat((y_true, labels[:, [0]]), 0)
+            all_outputs = torch.cat((all_outputs, outputs[:, [0]]), 0)
+
+            average_loss = valid_loss / (batch_idx + 1)
+            pbar_valid_desc(pbar_val, average_loss)
+
+    score = calc_score(y_true.cpu(), all_outputs.cpu())
+    return average_loss, score
+
+
+def epoch_end(avg_val_loss, best_score, score, model, save_path):
+    if score > best_score:
+        best_score = score
+        torch.save(model.state_dict(), save_path)
+        print(f"Val Loss: {avg_val_loss:.4f}  score: {score:.4f}\tSAVED MODEL\n")
+
+    else:
+        print(f"Val Loss: {avg_val_loss:.4f}  score: {score:.4f}\n")
+
+    return best_score
+
+
+def aux_train_pipeline(cfg):
+    set_seed(cfg.seed)
+
+    df = get_train_preprocess(cfg)(cfg)
+    train_loader, valid_loader = get_train_dataloader(df, 0, cfg)
+
+    model = get_model(cfg).to(DEVICE)
+    optimizer = get_optimizer(model, cfg)
+    scheduler = get_scheduler(optimizer, cfg)
+
+    best_score = 0
+
+    wandb.init(
+        project=COMP_NAME, name=cfg.exp_name, dir="/workspace/", mode=cfg.wandb_mode
+    )
+
+    for epoch in range(cfg.epochs):
+        train_loss = train_1epoch(model, train_loader, optimizer, scheduler, epoch, cfg)
+        valid_loss, score = valid_1epoch(model, valid_loader, epoch, cfg)
+
+        wandb.log(
+            {
+                "epoch": epoch,
+                "train_loss": train_loss,
+                "valid_loss": valid_loss,
+                "score": score,
+            }
+        )
+
+        model_path = Path(f"/kaggle/weights/{cfg.exp_name}/{cfg.exp_name}.pth")
+        model_path.parent.mkdir(parents=True, exist_ok=True)
+
+        best_score = epoch_end(valid_loss, best_score, score, model, model_path)
+
+
+def aux_infer_pipeline(cfg):
+    set_seed(cfg.seed)
+
+    df, df_sub = get_infer_preprocess(cfg)(cfg)
+    test_loader = get_test_dataloader(df, cfg)
+
+    model = get_model(cfg)
+    model.load_state_dict(torch.load(cfg.weight_path)).to(DEVICE)
+
+    preds = []
+    with torch.no_grad():
+        bar = tqdm(enumerate(test_loader), total=len(test_loader))
+        for step, data in bar:
+            images = data["image"].to(cfg["device"], dtype=torch.float)
+            outputs = model(images)
+            preds.append(outputs.detach().cpu().numpy())
+    preds = np.concatenate(preds).flatten()
+    df_sub["target"] = preds
+    df_sub.to_csv("submission.csv", index=False)
--- src/pipeline/common.py
+++ src/pipeline/common.py
@@ -1,3 +1,4 @@
+from .aux import aux_infer_pipeline, aux_train_pipeline
 from .base import base_infer_pipeline, base_train_pipeline
 
 
@@ -5,6 +6,8 @@ def get_train_pipeline(cfg):
     match cfg.pipeline:
         case "base":
             return base_train_pipeline
+        case "aux":
+            return aux_train_pipeline
         case _:
             raise ValueError(f"Invalid Pipeline Name: {cfg.pipeline}")
 
@@ -13,5 +16,7 @@ def get_infer_pipeline(cfg):
     match cfg.pipeline:
         case "base":
             return base_infer_pipeline
+        case "aux":
+            return aux_infer_pipeline
         case _:
             raise ValueError(f"Invalid Pipeline Name: {cfg.pipeline}")
