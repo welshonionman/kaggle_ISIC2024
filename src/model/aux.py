86615258969cd425fb84fd013c3d70a370641dec
welshonionman <welshonionman@gmail.com>
2024-07-25 15:17:30 +0000

    aux model

---
+++ src/model/aux.py
@@ -0,0 +1,57 @@
+import timm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class GeM(nn.Module):
+    def __init__(self, p=3, eps=1e-6):
+        super(GeM, self).__init__()
+        self.p = nn.Parameter(torch.ones(1) * p)
+        self.eps = eps
+
+    def forward(self, x):
+        return self.gem(x, p=self.p, eps=self.eps)
+
+    def gem(self, x, p=3, eps=1e-6):
+        return F.avg_pool2d(x.clamp(min=eps).pow(p), (x.size(-2), x.size(-1))).pow(
+            1.0 / p
+        )
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(p={self.p.data.tolist()[0]:.4f}, eps={self.eps})"
+
+
+class ISIC_Aux_Model(nn.Module):
+    def __init__(self, encoder_name, cfg):
+        super(ISIC_Aux_Model, self).__init__()
+        auxtarget = getattr(cfg, "auxtarget", [])
+
+        self.model = timm.create_model(encoder_name, pretrained=True)
+
+        in_features = self.model.classifier.in_features
+        self.model.classifier = nn.Identity()
+        self.model.global_pool = nn.Identity()
+        self.pooling = GeM()
+
+        self.linear_mal = nn.Linear(in_features, 1)
+        self.linear_sex = nn.Linear(in_features, 1)
+        self.linear_age = nn.Linear(in_features, 1)
+        self.linear_site = nn.Linear(in_features, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, images):
+        features = self.model(images)
+        pooled_features = self.pooling(features).flatten(1)
+
+        malignant = self.sigmoid(self.linear_mal(pooled_features))
+        sex = self.sigmoid(self.linear_sex(pooled_features))
+        age = self.sigmoid(self.linear_age(pooled_features))
+        site = self.sigmoid(self.linear_site(pooled_features))
+
+        return {
+            "malignant": malignant,
+            "sex": sex,
+            "age": age,
+            "site": site,
+        }
--- src/model/common.py
+++ src/model/common.py
@@ -1,13 +1,13 @@
+from .aux import ISIC_Aux_Model
 from .base import ISIC_Base_Model
 
 
 def get_model(cfg):
-    auxtarget = getattr(cfg, "auxtarget", [])
-    num_classes = len(auxtarget) + 1
-
     match cfg.model_name:
         case "base":
-            model = ISIC_Base_Model(cfg.encoder_name, num_classes=num_classes)
+            model = ISIC_Base_Model(cfg.encoder_name)
+        case "aux":
+            model = ISIC_Aux_Model(cfg.encoder_name, cfg)
         case _:
             raise ValueError(f"Invalid Model Name: {cfg.model_name}")
 
