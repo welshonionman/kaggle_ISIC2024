0e8396253f0b4b40ebda3deff9679b4cb39933da
welshonionman <welshonionman@gmail.com>
2024-07-25 03:19:47 +0000

    get_model num_classes

--- src/model/common.py
+++ src/model/common.py
@@ -2,10 +2,13 @@ from .base import ISIC_Base_Model
 
 
 def get_model(cfg):
+    auxtarget = getattr(cfg, "auxtarget", [])
+    num_classes = len(auxtarget) + 1
+
     match cfg.model_name:
         case "base":
-            model = ISIC_Base_Model(cfg.encoder_name)
+            model = ISIC_Base_Model(cfg.encoder_name, num_classes=num_classes)
         case _:
             raise ValueError(f"Invalid Model Name: {cfg.model_name}")
-        
+
     return model
